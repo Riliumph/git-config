54e6e6394b1b82772cb92d19f0ea624b8c65c30c
Rily <rilumph@yahoo.co.jp>
2017-04-24 02:57:02 +0900

    [ADD] git alias command

--- option.sh
+++ option.sh
@@ -1,3 +1,21 @@
+[alias]
+	branches = branch -a
+	rename-branch = branch -m
+	stashes = stash list
+	remotes = remote -v
+	log-tree = log --graph --abbrev-commit --date=short --pretty=format:'%C(green)[%cd] %C(cyan)<%an> %C(red)%h%C(yellow)%d %C(reset)%s '
+	time-line = log --graph -20 --branches --remotes --tags  --format=format:'%C(cyan)<%cN> %C(red)%h %C(reset)%<(75,trunc)%s %C(green)(%cr) %C(yellow)%d' --date-order
+	detail = status -uall
+	summary = status -s -u
+# work option alias
+	stage = add
+	unstage = reset HEAD
+	discard = checkout
+# commit = commit
+	uncommit = reset --mixed HEAD~
+	meldiff = difftool
+	melge = mergetool
+	staged-diff = diff --cached
 [core]
 	quotepath = false
 [pull]
