437d5812d47d8561ec0ef99a2e74e4185f998b07
Riliumph <riliumph@gmail.com>
2020-06-10 13:05:08 +0900

    [Add] new command: purify

--- option.sh
+++ option.sh
@@ -1,13 +1,14 @@
 [alias]
+# show option alias
 	branches = branch -a
 	rename-branch = branch -m
-	stashes = stash list
 	remotes = remote -v
 	log-tree = log --graph --abbrev-commit --date=short --pretty=format:'%C(green)[%cd] %C(cyan)<%an> %C(red)%h%C(yellow)%d %C(reset)%s '
 	time-line = log --graph -20 --branches --remotes --tags  --format=format:'%C(cyan)<%cN> %C(red)%h %C(reset)%<(75,trunc)%s %C(green)(%cr) %C(yellow)%d' --date-order
 	detail = status -uall
 	summary = status -s -u
 # work option alias
+	stashes = stash list
 	stage = add
 	unstage = reset HEAD
 	discard = checkout
@@ -16,6 +17,7 @@
 	meldiff = difftool
 	melge = mergetool
 	staged-diff = diff --cached
+	purify = clean -df
 [core]
 	quotepath = false
 [diff]
