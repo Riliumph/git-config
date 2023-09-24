23e5af4fe64d73a1d7037ea12e3509a0b44161bf
Riliumph <riliumph@outlook.jp>
2023-09-24 17:27:09 +0900

    [update] how to install diff-highlight

--- INSTALL.bash
+++ INSTALL.bash
@@ -4,7 +4,8 @@
 # Convert to absolute path
 # @param $1 path
 # @return absolute path
-abs_dirname() {
+abs_dirname()
+{
   local path="$1"
 
   # Check path existence one by one
@@ -23,18 +24,16 @@ abs_dirname() {
 user_bin="$HOME/bin"
 
 echo "Checking git ..."
-if ! type git &>/dev/null; then
+if ! type git &> /dev/null; then
   echo "Git is not installed"
   exit 1
 fi
 
 echo "Setting diff-highlight ..."
-if [ -e "${user_bin}/diff-highlight" ]; then
-  echo "  Skipped as the file already exists"
-else
+if ! type diff-highlight &> /dev/null; then
   echo "  Searching diff-highlight ..."
   search_key="diff-highlight"
-  highlight=("$(find /usr -type f -name ${search_key} 2>/dev/null)")
+  highlight=("$(find /usr -type f -name ${search_key} 2> /dev/null)")
   if [ -z "${highlight[0]}" ]; then
     echo "  Cannot find ${search_key}"
     exit 1
@@ -51,7 +50,7 @@ else
 fi
 
 echo "Setting bash ..."
-echo 'export PATH="$HOME/bin:${PATH}"' >>"$HOME/.bashrc"
+echo 'export PATH="$HOME/bin:${PATH}"' >> "$HOME/.bashrc"
 echo '[include]
-    path = '$(abs_dirname "$0")'/option.sh' >>"$HOME/.gitconfig"
+    path = '$(abs_dirname "$0")'/option.sh' >> "$HOME/.gitconfig"
 echo "DONE!!"
